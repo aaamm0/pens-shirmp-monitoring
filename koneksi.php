1a0e7ad21b4afe91fec74fd2c40203584fe3a411
aam <muhammadaam0@gmail.com>
2025-04-18 13:50:00 +0700

    update

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";   
-$username = "root";         
-$password = "";            
-$dbname = "shirmpcare";    
+$username = "aam";         
+$password = "aam123";            
+$dbname = "aam_shirmp";    
 $conn = new mysqli($servername, $username, $password, $dbname);
  
 if ($conn->connect_error) {
