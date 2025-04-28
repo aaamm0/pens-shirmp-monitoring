9c148a9ec4ce6522f097a94692f249ef995f2f64
aam <muhammadaam0@gmail.com>
2025-04-28 21:28:34 +0700

    update

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,13 @@
 <?php
 // date_default_timezone_set("Asia/Jakarta");
-// $servername = "localhost";   
-// $username = "aam";         
-// $password = "aam123";            
-// $dbname = "aam_shirmp";    
 $servername = "localhost";   
-$username = "root";         
-$password = "";            
-$dbname = "shirmpcare";    
+$username = "aam";         
+$password = "aam123";            
+// $dbname = "aam_shirmp";    
+// $servername = "localhost";   
+// $username = "root";         
+// $password = "";            
+// $dbname = "shirmpcare";    
 
 $conn = new mysqli($servername, $username, $password, $dbname);
  
